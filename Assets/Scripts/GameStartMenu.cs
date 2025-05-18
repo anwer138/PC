8f47755d80fdccc365a3ae0519713ab86333041d
aki414 <akremsedki21@gmail.com>
2025-05-18 21:59:41 +0100

    FIX

--- Assets/Scripts/GameStartMenu.cs
+++ Assets/Scripts/GameStartMenu.cs
@@ -7,12 +7,10 @@ public class GameStartMenu : MonoBehaviour
 {
     [Header("UI Pages")]
     public GameObject mainMenu;
-    public GameObject options;
     public GameObject about;
 
     [Header("Main Menu Buttons")]
     public Button startButton;
-    public Button optionButton;
     public Button aboutButton;
     public Button quitButton;
 
@@ -25,7 +23,6 @@ public class GameStartMenu : MonoBehaviour
 
         //Hook events
         startButton.onClick.AddListener(StartGame);
-        optionButton.onClick.AddListener(EnableOption);
         aboutButton.onClick.AddListener(EnableAbout);
         quitButton.onClick.AddListener(QuitGame);
 
@@ -49,26 +46,18 @@ public class GameStartMenu : MonoBehaviour
     public void HideAll()
     {
         mainMenu.SetActive(false);
-        options.SetActive(false);
         about.SetActive(false);
     }
 
     public void EnableMainMenu()
     {
         mainMenu.SetActive(true);
-        options.SetActive(false);
         about.SetActive(false);
     }
-    public void EnableOption()
-    {
-        mainMenu.SetActive(false);
-        options.SetActive(true);
-        about.SetActive(false);
-    }
+    
     public void EnableAbout()
     {
         mainMenu.SetActive(false);
-        options.SetActive(false);
         about.SetActive(true);
     }
 }
